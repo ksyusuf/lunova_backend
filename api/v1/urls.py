e141c624f6d3a86f0fa882af07c19ca14e3f90ba
ksyusuf <ksyusuf85@gmail.com>
2025-07-22 16:04:53 +0300

    cache temizlik

--- api/v1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('accounts/', include('api.v1.accounts.urls')),  # 👈 v1/accounts
-    # path('experts/', include('api.v1.experts.urls')), gibi ileride genişletilir
-]
